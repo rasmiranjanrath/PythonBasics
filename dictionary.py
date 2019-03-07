4f116ad2c007f66853ae2afb509fe653b363cd6f
Rasmiranjan Rath <rasmiranjan.rath@girmiti.com>
2019-03-07 12:27:43 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+dict={'name':'rasmiranjan','designation':'programmer','age':25}
+print(dict['name'])
+print(dict['designation'])
+print(dict['age'])
--- primeno.py
+++ primeno.py
@@ -7,3 +7,14 @@ while(i < PRIME_NO_LIMIT):
       j = j + 1
    if (j > i/j) : print i, " is prime"
    i = i + 1
+
+#another way 
+a=int(input("Enter number: "))
+k=0
+for i in range(2,a//2+1):
+    if(a%i==0):
+        k=k+1
+if(k<=0):
+    print("Number is prime")
+else:
+    print("Number isn't prime")
