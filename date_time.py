1e46771ed4c9b991fb8876d8f2392bf318665f47
Rasmiranjan Rath <rasmiranjan.rath@girmiti.com>
2019-03-08 15:05:06 +0530

    file datetime variable type

---
+++ date_time.py
@@ -0,0 +1,9 @@
+#printing todays date
+import time as t
+time = t.asctime(t.localtime(t.time()))
+print (time)
+
+#print a calander
+import calendar
+cal=calendar.month(2019,3)
+print cal
---
+++ variables.py
@@ -0,0 +1,8 @@
+import math
+a=10
+def var_test():
+	a=20
+	content=dir(math)
+	print(content)
+	print(a)
+var_test()
